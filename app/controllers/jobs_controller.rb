8357bc501b54b706adc1fe36d07fa55860c0be50
wangxing <18912540921@163.com>
2017-04-07 22:12:50 +0800

    n

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
   def index
     @jobs = Job.all
   end
@@ -23,10 +24,12 @@ class JobsController < ApplicationController
 
   def edit
     @job = Job.find(params[:id])
+
   end
 
   def update
     @job = Job.find(params[:id])
+
     if @job.update(job_params)
       redirect_to jobs_path ,noteice: "Job updated!"
     else
@@ -36,12 +39,15 @@ class JobsController < ApplicationController
 
   def destroy
     @job = Job.find(params[:id])
+
     @job.destroy
     redirect_to jobs_path
   end
 
 
   private
+
+
 
   def job_params
     params.require(:job).permit(:title,:description)
